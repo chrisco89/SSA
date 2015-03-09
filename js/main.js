5343e8073b2de0de709ab0775ec84da3e3433e5b
Christian Cerda Österlind <christian@Christians-MacBook-Pro.local>
2015-03-09 08:45:13 +0100

    First Commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('document').ready( function() {
+	
+	$('.addimage').click( function(e) {
+		$(this).slideUp('fast');
+		$(this).next().slideDown('fast');
+		e.preventDefault();
+	});
+	
+	$('.lightbox .close, .lightbox .overlay, .lightbox .btn').click( function() {
+		$(this).parents('.lightbox').fadeOut();
+	});
+	
+	$('.uploader').click( function() {
+		$(this).hide();
+		$(this).next().fadeIn('slow');
+		$('input.focus').focus();
+		$('.existing').hide();
+	});
+	
+	$('table .thumb').click( function() {
+		$('.lightbox.image').fadeIn();
+	});
+	
+	$('td.date').click( function() {
+		$(this).attr('contentEditable', 'true');
+	});
+	
+});
