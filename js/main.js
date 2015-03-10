58cdbe9a467125c07e79bc6b4044f784e680488e
Christian Cerda Österlind <christian@Christians-MacBook-Pro.local>
2015-03-10 08:13:55 +0100

    faq etc

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ $('document').ready( function() {
 		$(this).next().slideDown('fast');
 		e.preventDefault();
 	});
-	$('.logout .dropdown').mouseleave( function() {
-		$(this).slideUp('fast');
+	$('.logout').mouseleave( function() {
+		$(this).find('.dropdown').slideUp('fast');
 	});
 	
 	$('.addimage').click( function(e) {
@@ -32,6 +32,23 @@ $('document').ready( function() {
 	
 	$('td.date').click( function() {
 		$(this).attr('contentEditable', 'true');
+	});
+	
+	$('#bottompanel .add').click( function(e) {
+		$('.lightbox.adding').fadeIn('fast');
+		$('.adding .addschool').attr('href', '/logged-in-2.html');
+		e.preventDefault();
+	});
+	
+	$('.showfaq').click( function() {
+		$('.wrapper').toggleClass('faqactive');
+	});
+	
+	
+	$('.btn-holder.send').click( function(e) {
+		$('.module.sendexample table, .module.sendexample .btn, .module.sendexample .uploader, .module.sendexample .footer').hide();
+		$('.module.sendexample .sent').fadeIn('fast');
+		e.preventDefault();
 	});
 	
 	
